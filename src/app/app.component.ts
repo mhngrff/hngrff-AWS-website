47385a2e72646798304bcdfa69b784f071e7f941
mhngrff <matt.hanagriff@gmail.com>
2024-10-13 19:13:22 -0400

    basic footer code added, testing mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet, NavigationEnd, Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent} from './navbar/navbar.component'
+import { FooterComponent } from './footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
