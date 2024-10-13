0441538cc3277935d9c03cf1f060f948e885ce57
mhngrff <matt.hanagriff@gmail.com>
2024-10-13 02:21:43 -0400

    routing related changes, not a full solution

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  template: '<h1>Contact Component</h1>',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
