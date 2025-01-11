704cec7a22b98620fbd6e760575f4652cae14090
mhngrff <matt.hanagriff@gmail.com>
2025-01-11 18:29:09 -0500

    loading state added to success page

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -16,6 +16,9 @@ export class SuccessComponent implements OnInit {
   orderDetails: any;
   isLoading = true;
   errorMessage: string | null = null;
+  spinnerText = 'Fetching your order';
+
+  private spinnerInterval: any;
 
   constructor(
     private ordersService: OrdersService,
@@ -25,6 +28,7 @@ export class SuccessComponent implements OnInit {
     ) {}
 
   ngOnInit() {
+    this.startSpinner();
 
     const orderId = sessionStorage.getItem('orderId');
     console.log("Reached success component with orderId: ", orderId);
@@ -40,6 +44,7 @@ export class SuccessComponent implements OnInit {
           (orderDetails) => {
             this.orderDetails = orderDetails;
             this.isLoading = false;
+            this.stopSpinner();
             console.log('Fetched order details:', this.orderDetails);
           },
           (error) => {
@@ -59,4 +64,26 @@ export class SuccessComponent implements OnInit {
         }
       });
     }
+
+    startSpinner(): void {
+      let dotCount = 0;
+
+      this.spinnerInterval = setInterval(() => {
+        dotCount = (dotCount + 1 ) % 4;
+        const dots = '.'.repeat(dotCount);
+        this.spinnerText = `Fetching your order${dots}`;
+        }, 150); //150ms
+      }
+
+      stopSpinner(): void {
+        if (this.spinnerInterval) {
+          clearInterval(this.spinnerInterval);
+          this.spinnerInterval = null;
+        }
+      }
+
+      ngOnDestroy() {
+        this.stopSpinner();
+      }
+
   }
