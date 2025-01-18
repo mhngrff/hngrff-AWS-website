1db026b29cdca05ef6673b5cae8f58a4ce89dd22
mhngrff <matt.hanagriff@gmail.com>
2025-01-18 16:10:45 -0500

    testing new yml file, environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  stripeKey: 'your-stripe-live-key', // Replace with live key
+  stripeKey: 'pk_live_51QD4EFCkan3FkVYDz3xpFJeQK2zIMVmg3frK1VaTPP5NTKEPzEERSbOfboIsLiYKZb1PPjL7lnkbtmMPceYnW8vw00wOc64NPL',
   googleMapsApiKey: 'AIzaSyC2M6pU6cZVbtrrnb2t3KmtwdvZ4Q6cbiU'
 };
