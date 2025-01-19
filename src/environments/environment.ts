c4aeeba1be1b08d076ef206525876cb5a661eb85
mhngrff <matt.hanagriff@gmail.com>
2025-01-18 19:15:05 -0500

    hardcoding ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  stripePublicKey: 'pk_test_51QD4EFCkan3FkVYDXSZLVEO2msoiyEuOi7M6zZqcKS9HKHrGsk2Q2UhlmDU5lhhQjo6NxVyEXhFt2JqMAu2DBlEo00AzCcNRdA',
+//   stripePublicKey: 'pk_test_51QD4EFCkan3FkVYDXSZLVEO2msoiyEuOi7M6zZqcKS9HKHrGsk2Q2UhlmDU5lhhQjo6NxVyEXhFt2JqMAu2DBlEo00AzCcNRdA',
+  stripePublicKey: 'pk_live_51QD4EFCkan3FkVYDz3xpFJeQK2zIMVmg3frK1VaTPP5NTKEPzEERSbOfboIsLiYKZb1PPjL7lnkbtmMPceYnW8vw00wOc64NPL',
   googleMapsApiKey: 'AIzaSyC2M6pU6cZVbtrrnb2t3KmtwdvZ4Q6cbiU'
 };
