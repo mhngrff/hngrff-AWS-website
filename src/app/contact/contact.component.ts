f7202aa2e2d90153e7ba5bad358ec012598b7e12
mhngrff <matt.hanagriff@gmail.com>
2025-01-19 14:23:26 -0500

    contact page basic implementation, minor style changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  isShippingVisible = false;
+  isFramingVisible = false;
+  isTrackingVisible = false;
+
+  toggleShipping(): void {
+    this.isShippingVisible = !this.isShippingVisible;
+  }
+
+  toggleTracking(): void {
+    this.isTrackingVisible = !this.isTrackingVisible;
+  }
+
+  toggleFraming(): void {
+    this.isFramingVisible = !this.isFramingVisible;
+  }
 
 }
